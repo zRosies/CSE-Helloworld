81015bbc976069b618589e221771f39fd415e6ec
zRosies <gustavo.enrollment@gmail.com>
2024-06-04 21:29:40 -0300

    Hello world Software

--- hello.js
+++ hello.js
@@ -1,5 +1,9 @@
-const newElement = document.createElement("h1");
-newElement.id = "first";
-document.body.appendChild(newElement);
-const h1 = document.querySelector("#first");
-h1.innerHTML = " Hello World :)";
+function displayHello() {
+  const newElement = document.createElement("h1");
+  newElement.id = "first";
+  document.body.appendChild(newElement);
+  const h1 = document.querySelector("#first");
+  h1.innerHTML = " Hello World :)";
+}
+
+displayHello();
